c855ac04fa1917f7e078cf78a5fc831709d3698b
tuna2134 <me@tuna2134.dev>
2023-05-30 11:15:38 +0000

    あ

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 use poise::serenity_prelude as serenity;
+use sqlx::sqlite::SqlitePool;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+struct Data {
+    pool: SqlitePool,
+} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command)]
-async fn age(
+async fn join(
     ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
+    let pool = &ctx.data().pool;
+    let channel_id = ctx.channel_id().0 as i64;
+    sqlx::query!("INSERT INTO Channels VALUES (?)", channel_id)
+        .execute(pool)
+        .await
+        .expect("Failed to insert channel into database");
+    ctx.say("Channel added to database").await?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
+    let pool = SqlitePool::connect((std::env::var("DATABASE_URL").unwrap()).as_str())
+        .await
+        .unwrap();
+    sqlx::migrate!().run(&pool).await.unwrap();
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![join()],
             ..Default::default()
         })
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
@@ -28,9 +38,10 @@ async fn main() {
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {
+                    pool: pool.clone(),
+                })
             })
         });
-
     framework.run().await.unwrap();
 }
