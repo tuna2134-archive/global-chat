caffb6d7154d18ee952ff362d7ddbbc367023b1e
mc_fdc <me@tuna2134.dev>
2023-06-01 11:25:36 +0900

    Update App.tsx

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="text-2xl">Hello</h1>
+        <div className="px-3 sm:px-4.5 w-full flex max-w-4xl mx-auto py-4 items-center">
+          <h1 className="flex text-2xl">Hello</h1>
+        </div>
       </header>
     </>
   )
